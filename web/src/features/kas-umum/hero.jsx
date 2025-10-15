18c459154b35176ac4c17caf1b14d23429139c2b
Andre <andhiki28@gmail.com>
2025-10-15 16:21:01 +0700

    feat: some files

---
+++ web/src/features/kas-umum/hero.jsx
@@ -0,0 +1,9 @@
+export default function Hero() {
+  return (
+    <div>
+      <h1>Welcome to Bank Desa</h1>
+      <p>Explore our services</p>
+      dsads
+    </div>
+  );
+}
---
+++ web/src/features/kas-umum/index.jsx
@@ -0,0 +1,9 @@
+export default function KasUmum() {
+  return (
+    <div>
+      <h1>Welcome to Kas Umum</h1>
+      <p>Explore our services</p>
+      dsads
+    </div>
+  );
+}
