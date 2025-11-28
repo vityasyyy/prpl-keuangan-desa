1f9353354cfed500584d8df0a4e497a3a21ff95b
david-irvinne <davidneilleen@gmail.com>
2025-11-28 11:53:42 +0700

    fix: resolve conflict message

--- backend/src/api/router/container.router.js
+++ backend/src/api/router/container.router.js
@@ -1,12 +1,7 @@
 // import apbdRouter from "./apbd/apbd.router.js";
 // import bankDesaRouter from "./bank-desa/bank-desa.router.js";
-<<<<<<< HEAD
-import kasPembantuRouter from "./kas-pembantu/kas-pembantu.router.js";
-// import createKasUmumRouter from "./kas-umum/kas-umum.router.js";
-=======
 // import kasPembantuRouter from "./kas-pembantu/kas-pembantu.router.js";
 import createKasUmumRouter from "./kas-umum/kas-umum.router.js";
->>>>>>> origin/dev
 // import rabRouter from "./rab/rab.router.js";
 import createAuthRouter from "./auth/auth.router.js";
 
@@ -16,12 +11,7 @@ export function initializeRoutes(app, handlers) {
 
   // app.use('/api/apbd', apbdRouter(handlers.apbdHandler));
   // app.use('/api/bank-desa', bankDesaRouter(handlers.bankDesaHandler));
-<<<<<<< HEAD
-  app.use('/api/kas-pembantu', kasPembantuRouter(handlers.kasPembantuHandler));
-  // app.use('/api/kas-umum', createKasUmumRouter(handlers.kasUmumHandler));
-=======
   // app.use('/api/kas-pembantu', kasPembantuRouter(handlers.kasPembantuHandler));
   app.use("/api/kas-umum", createKasUmumRouter(handlers.kasUmumHandler));
->>>>>>> origin/dev
   // app.use('/api/rab', rabRouter(handlers.rabHandler));
 }
